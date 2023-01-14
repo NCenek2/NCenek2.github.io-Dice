e764e77732d6de295b5f7cd43d67375ef0ba275a
NCenek2 <ceneknicholas@gmail.com>
2023-01-14 17:08:31 -0500

    Btn refresh/ mobile sizing
    Add btn to refresh die roll/ consistent for mobile

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Generates a random number from 1 to 6
 function randomNumber() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+// Takes the player number and returns a new class name
 function handleDie(number) {
   const numberStrings = ["one", "two", "three", "four", "five", "six"];
   const numberString = numberStrings[number - 1];
@@ -9,6 +11,7 @@ function handleDie(number) {
   return newClass;
 }
 
+// Takes the die number and returns the corresponding inner HTML
 function handleDot(number) {
   const numberStrings = ["one", "two", "three", "four", "five", "six"];
   const numberString = numberStrings[number - 1];
@@ -18,19 +21,24 @@ function handleDot(number) {
   }
   return newHTML;
 }
-
+// Handles the winner of the roll of die
 function handleWinner(number1, number2) {
   let titleString = "";
   if (number1 > number2) {
-    titleString = "🚩 Sam Wins!";
+    titleString = "🚩 Player 1 Wins!";
   } else if (number2 > number1) {
-    titleString = "🚩 Nick Wins!";
+    titleString = "🚩 Player 2 Wins!";
   } else {
-    titleString = "🚩 Draw 🐶";
+    titleString = "🚩 Draw!";
   }
   return titleString;
 }
 
+function handleReset() {
+  location.reload();
+}
+
+// Taking new classes and new HTML and implementing into DOM
 const player1Number = randomNumber();
 const player2Number = randomNumber();
 document.querySelector(".player1-die").classList.add(handleDie(player1Number));
@@ -38,5 +46,11 @@ document.querySelector(".player1-die").innerHTML = handleDot(player1Number);
 document.querySelector(".player2-die").classList.add(handleDie(player2Number));
 document.querySelector(".player2-die").innerHTML = handleDot(player2Number);
 
+// Implementing Winner Text into DOM
 const headerText = (document.querySelector(".header").textContent =
   handleWinner(player1Number, player2Number));
+
+// Handling Reset to Roll Die Again
+const dieButton = document.querySelector(".dice-btn");
+dieButton.addEventListener("click", handleReset);
+dieButton.removeEventListener("click", handleReset);
